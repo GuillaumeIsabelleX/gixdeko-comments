40e4ceead6c14d4cdfba0cc728a2b8dc917b9673
Guillaume Isabelle <jgi@jgwill.com>
2019-04-20 09:52:31 -0400

    sample colorization

---
+++ sample-dekos.js
@@ -0,0 +1,23 @@
+//@Vision Visually Meaningful colors to point out what we are doing in our code :)
+//@due 
+//@a That our actions are identifiable
+//@cr Talk about what is really hapenning so that we know where we are in relationship to the vision.
+//@state Any state we are in?
+//@s Any state we are in?
+
+//@feature Meaningful Colorization of Code
+//@urir http://robertfritz.com
+
+//@promise returned
+
+//@rejecting Because the Database server could not be reached
+
+//@resolving Data was valid
+
+
+//@issue Handle exception on server disconnected
+
+//@bug Buggy part require review
+
+
+//@insight See : http://google.com to search
